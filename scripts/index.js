dfbbc494f87b05943e99c1b65f64dca8a739898b
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-13 14:03:35 -0600

    Added generateAddSortButtons function which generates the add and sort buttons IF store.erasedAddFilterMark is not true.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,23 @@
 'use strict';
 /* global, api, store.js */
 const bookMarkList = (function(){
-    const testResponse = 'I work';
+    function generateAddSortButtons(){
+        if(!store.erasedAddFilterMark){
+            return `<div role = 'container' class = 'wide container add-rating'>
+            <button>Add (book)Mark</button>
+            <form>
+                <label>Display Ratings Above:</label>
+                <select>           
+                    <option value="1">1 Star</option>
+                    <option value="2">2 Stars</option>
+                    <option value="3">3 Stars</option>
+                    <option value="4">4 Stars</option>
+                    <option value="5">5 Stars</option> 
+                </select>
+            </form>
+        </div>`;
+        }
+      }
     function generateBookMark(bookMarks){
         //should be able to handle edit and expanded formats
         return (`<div role = 'container' id = 'input later' class ='bookmark'>
@@ -17,20 +33,19 @@ const bookMarkList = (function(){
       </div>`);
       }
     function render() {
+        const addFilter = generateAddSortButtons();
         const bookMarks = store.items.map(bookMark => bookMarkList.generateBookMark(bookMark));
+        $('body').append(addFilter);
         $('body').append(bookMarks);
     };
     return {
-        testResponse,
         generateBookMark,
+        generateAddSortButtons,
         render
     };
 }());
 
 bookMarkList.render();
 
-/*
-function generateAddSortButtons(){
 
-}
-*/
+
--- scripts/store.js
+++ scripts/store.js
@@ -4,12 +4,12 @@
 const store = (function(){
   const items =[{title: 'MarkBook', url:'www.google.com', desc: 'This is a bookmark', rating: 3, expanded: true, edit: false},
     {title: 'Mark\'s Books', url: 'www.facebook.com', desc: 'cesspool', rating: 1, expanded: true, edit: false}];
-  let addMark = false;
+  let erasedAddFilterMark = true;
   let displayRating = 1;
   let sErr = '';
   return {
     items,
-    addMark,
+    erasedAddFilterMark,
     displayRating,
     sErr
   };
