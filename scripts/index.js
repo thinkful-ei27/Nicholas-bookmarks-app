400e1dd373a88f94ce226d7729b8676b4021d70e
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-13 13:54:59 -0600

    render now appends the bookmarks to the body element. Can handle expanded vs unexpanded bookmarks.

--- scripts/index.js
+++ scripts/index.js
@@ -4,22 +4,33 @@ const bookMarkList = (function(){
     const testResponse = 'I work';
     function generateBookMark(bookMarks){
         //should be able to handle edit and expanded formats
-        return ` <div role = 'container' id = 'input later' class ='bookmark'>
+        return (`<div role = 'container' id = 'input later' class ='bookmark'>
         <h3>${bookMarks.title}</h3>
-        <p role = 'rating'>${bookMarks.rating}</p>
-        <button>Edit (book)Mark</button>
+        <p role = 'rating'>${bookMarks.rating}</p>`
+        + 
+        (bookMarks.expanded ? 
+        `<p>${bookMarks.desc}</p>
+        <button href=${bookMarks.url}>(book)Mark URL</button>` : '') 
+        + 
+        `<button>Edit (book)Mark</button>
         <button>Delete (book)Mark</button>
-      </div>`;
+      </div>`);
       }
+    function render() {
+        const bookMarks = store.items.map(bookMark => bookMarkList.generateBookMark(bookMark));
+        $('body').append(bookMarks);
+    };
     return {
         testResponse,
-        generateBookMark
+        generateBookMark,
+        render
     };
 }());
 
-function render() {
-    const bookMarks = store.items.map(bookMark => bookMarkList.generateBookMark(bookMark));
-    $('body').append(bookMarks);
-}
+bookMarkList.render();
 
-render();
+/*
+function generateAddSortButtons(){
+
+}
+*/
--- scripts/store.js
+++ scripts/store.js
@@ -2,8 +2,8 @@
 /* global, index, api */
 
 const store = (function(){
-  const items =[{title: 'MarkBook', url:'www.google.com', desc: 'This is a bookmark', rating: 3, expanded: false, edit: false},
-    {title: 'Mark\'s Books', url: 'www.facebook.com', desc: 'cesspool', rating: 1, expanded: false, edit: false}];
+  const items =[{title: 'MarkBook', url:'www.google.com', desc: 'This is a bookmark', rating: 3, expanded: true, edit: false},
+    {title: 'Mark\'s Books', url: 'www.facebook.com', desc: 'cesspool', rating: 1, expanded: true, edit: false}];
   let addMark = false;
   let displayRating = 1;
   let sErr = '';
