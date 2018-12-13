81029b730b023f31d7151b5d1a605ad2033bc54f
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-13 13:33:52 -0600

    Added generateBookMark function to bookMarkList IFFE. Currently returns the closed version of a bookmark

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,28 @@
 'use strict';
+/* global, api, store */
 const bookMarkList = (function(){
     const testResponse = 'I work';
-
+    function generateBookMark(bookMarks){
+        return ` <div role = 'container' id = 'input later' class ='bookmark'>
+        <h3>${bookMarks.title}</h3>
+        <p role = 'rating'>${bookMarks.rating}</p>
+        <button>Edit (book)Mark</button>
+        <button>Delete (book)Mark</button>
+      </div>`;
+      }
     return {
-        testResponse
+        testResponse,
+        generateBookMark
     };
 }());
+
+//generateBookMark will need to factor in expanded, maybe edit?
+function generateBookMark(bookMarks){
+  return ` <div role = 'container' id = 'input later' class ='bookmark'>
+  <h3>${bookMarks.title}</h3>
+  <p role = 'rating'>${bookMarks.rating}</p>
+  <button>Edit (book)Mark</button>
+  <button>Delete (book)Mark</button>
+</div>`;
+}
+
