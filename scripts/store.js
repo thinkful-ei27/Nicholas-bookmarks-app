ca3b9633ec15e48f92a1fa9c3862f28b560bcc3e
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-13 14:57:47 -0600

    Tried to add cuid. Gave up (temporarily).

--- scripts/store.js
+++ scripts/store.js
@@ -1,9 +1,9 @@
 'use strict';
-/* global, index, api */
+/* global, index, api*/
 
 const store = (function(){
-  const items =[{title: 'MarkBook', url:'www.google.com', desc: 'This is a bookmark', rating: 3, expanded: true, edit: true},
-    {title: 'Mark\'s Books', url: 'www.facebook.com', desc: 'cesspool', rating: 1, expanded: false, edit: false}];
+  const items =[{title: 'MarkBook', url:'www.google.com', desc: 'This is a bookmark', rating: 3, id: '34509sdjsdfgike093245', expanded: true, edit: true},
+    {title: 'Mark\'s Books', url: 'www.facebook.com', desc: 'cesspool', rating: 1, id: '435989ujdfnjk233r', expanded: false, edit: false}];
   let erasedAddFilterMark = true;
   let displayRating = 1;
   let sErr = '';
